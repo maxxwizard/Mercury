48796b90400b2d171d91ac3da88482eeaf2e4562
maxxwizard <matthewhuynh88@gmail.com>
2016-12-09 02:24:14 -0500

    working on cat fact

--- ProjectMercury/Controllers/HomeController.cs
+++ ProjectMercury/Controllers/HomeController.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
+using System.Runtime.Serialization.Json;
+using ProjectMercury.Helpers;
 
 namespace ProjectMercury.Controllers
 {
@@ -10,7 +14,37 @@ namespace ProjectMercury.Controllers
     {
         public ActionResult Index()
         {
+            
+
             return View();
+        }
+
+        private async void GetCatFact()
+        {
+            // assume failure
+            ViewBag.RandomCatFact = "Darn, we couldn't get a cat fact for you...";
+
+            // make a call to cat fact API to retrieve
+            HttpResponseMessage response = null;
+            var clientHandler = new HttpClientHandler();
+            var client = new HttpClient(clientHandler);
+            client.BaseAddress = new Uri("http://catfacts-api.appspot.com");
+            string targetUrl = client.BaseAddress.ToString() + "/api/facts";
+            try
+            {
+                response = await client.GetAsync(targetUrl);
+                string json = await response.Content.ReadAsStringAsync();
+                CatFact catFactResponse = System.Web.Helpers.Json.Decode(json);
+                ViewBag.RandomCatFact = catFactResponse;
+            }
+            catch
+            {
+                // do nothing
+                return;
+            }
+
+            // if we've gotten here, we failed for sure...
+
         }
 
         public ActionResult About()
---
+++ ProjectMercury/Helpers/CatFact.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.Serialization;
+
+namespace ProjectMercury.Helpers
+{
+    public class CatFact
+    { 
+
+        [DataContract]
+        public class Response
+        {
+            [DataMember(Name = "facts")]
+            public string Fact { get; set; }
+
+            [DataMember(Name = "success")]
+            public string Success { get; set; }
+        }
+    }
+}
